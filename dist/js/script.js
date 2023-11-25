7bc59f0b1604614b4682d9c779a6f189391d9432
Qunthunnan <bredtv6@gmail.com>
2023-11-25 20:36:57 +0300

    removed jquery

--- dist/js/script.js
+++ dist/js/script.js
@@ -1051,15 +1051,4 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-
-    // scroll
-    function scroll(item) {
-        $(item).click(function(){
-            const _href = $(item).attr("href");
-            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-            return false;
-        });
-    }
-
-    scroll($("a[href=#waiting]"));
 });
